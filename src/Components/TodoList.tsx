3f67c1b84847f7720aa830b050d179c92c96fe4a
SantamRC <santamdev404@gmail.com>
2023-05-08 15:34:26 +0530

    Added a Basic UI for Tasks

---
+++ src/Components/TodoList.tsx
@@ -0,0 +1,45 @@
+// TodoList.tsx
+import { FC } from 'react';
+import { useQuery } from 'react-query';
+import { List, ListItem, ListItemIcon, ListItemText } from '@mui/material';
+import { CheckCircleOutline, RadioButtonUnchecked } from '@mui/icons-material';
+import axios from 'axios';
+
+interface Todo {
+  _id: string;
+  title: string;
+  description: string;
+  status: boolean;
+}
+
+const fetchTodos = async () => {
+  const { data } = await axios.get('http://localhost:4000/api/todos?limit=10&page=1');
+  return data.todos as Todo[];
+};
+
+const TodoList: FC = () => {
+  const { data: todos, isLoading } = useQuery('todos', fetchTodos);
+
+  if (isLoading) {
+    return <div>Loading...</div>;
+  }
+
+  return (
+    <List>
+      {todos?.map((todo) => (
+        <ListItem key={todo._id}>
+          <ListItemIcon>
+            {todo.status ? (
+              <CheckCircleOutline color="primary" />
+            ) : (
+              <RadioButtonUnchecked color="primary" />
+            )}
+          </ListItemIcon>
+          <ListItemText primary={todo.title} secondary={todo.description} />
+        </ListItem>
+      ))}
+    </List>
+  );
+};
+
+export default TodoList;
--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { QueryClient, QueryClientProvider } from 'react-query';
+import { ReactQueryDevtools } from 'react-query/devtools';
+
 import '@fontsource/roboto/300.css';
 import '@fontsource/roboto/400.css';
 import '@fontsource/roboto/500.css';
@@ -7,12 +10,17 @@ import '@fontsource/roboto/700.css';
 import './index.css';
 import App from './App';
 
+const queryClient = new QueryClient();
+
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
   <React.StrictMode>
+    <QueryClientProvider client={queryClient}>
     <App />
+    <ReactQueryDevtools initialIsOpen={false} />
+  </QueryClientProvider>
   </React.StrictMode>
 );
 
