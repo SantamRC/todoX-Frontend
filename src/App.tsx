87852ad6e885432c38a9130f31b7ea5606b86caa
SantamRC <santamdev404@gmail.com>
2023-05-08 15:47:08 +0530

    Added AppBar

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,17 @@
 import { FC } from 'react';
 import { Container } from '@mui/material';
 import TodoList from './Components/TodoList';
+import AppBar from './Components/AppBar'
 
 const App: FC = () => {
   return (
+    <>
+    <AppBar /> 
     <Container maxWidth="md">
-      <h1>TodoX</h1>
-      <TodoList />
-    </Container>
+    <TodoList />
+  </Container>
+  </>
+   
   );
 };
 
